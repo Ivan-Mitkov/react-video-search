4f1d76b7059d6eb335b932a9300fca8d90738565
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-09-29 09:29:57 +0300

    add searchbar component

---
+++ src/components/search_bar.js
@@ -0,0 +1,34 @@
+
+import React, { Component } from 'react';
+
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { term: '' };
+
+    }
+    render() {
+        return (
+            <div>
+                {/* create input element and pass prop onChange with the value of reference to this.onInputChange  */}
+                <input
+                // Controlled component
+                    value={this.state.term}
+                    onChange={this.onInputChange}>
+                </input>
+                <div>Value of the input:{this.state.term}</div>
+            </div>
+        )
+    }
+
+    //declare event handler
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value });
+        // console.log(this.state);
+    }
+
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 
-import React  from 'react';
-import  ReactDOM  from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
 
 //AIzaSyAkiVy4FcsBskXy6kkS1j6KrVdHJuE16WE
-const API_KEY ='AIzaSyAkiVy4FcsBskXy6kkS1j6KrVdHJuE16WE';
+const API_KEY = 'AIzaSyAkiVy4FcsBskXy6kkS1j6KrVdHJuE16WE';
 //create ne component
 const App = () => {
     return (
         <div>
-            Hi
-    </div>
+            <SearchBar />
+        </div>
+
     )
 }
 
 
 //put this component on the page
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
 
